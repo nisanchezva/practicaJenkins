1147b8d8f7872ebafa54a0bd51d586b6337f1eca
Nicolas Sanchez Vargas <nisanchezva@unal.edu.co>
2024-12-26 11:57:19 -0500

    AMI actualizada

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "my_vm" {
-  ami           = "ami-065deacbcaac64cf2"  # Ubuntu 20.04 LTS 
+  ami           = "ami-0c2b8ca1dad447f8a"  # Ubuntu 20.04 LTS 
   instance_type = "t2.micro"  # Free Tier
 }
